f4c3762b1af45ac9fc67cd9cfa049a64c37e2cde
NourEldin Elkhatib <nimom6055@gmail.com>
2026-07-12 07:58:15 +0300

    Initialize password flow setup

--- src/app.py
+++ src/app.py
@@ -1,11 +1,12 @@
 # Imports the async context manager decorator from the contextlib module
 from contextlib import asynccontextmanager
+from typing import Annotated
 
 # Use the fastapi module (From the FastAPI lib) to import the FastAPI class (that inherits from Starlette) to provides the API functionality alongside the HTTPException class to handle returning HTTP errors
-from fastapi import FastAPI, HTTPException, Query, Request
+from fastapi import Depends, FastAPI, HTTPException, Query, Request, status
 from src.lead import Lead, LeadPublic, LeadCreate, LeadUpdate, LeadPatch, PaginatedResponse
 from src.db import create_db_and_tables, engine, Session, select, SessionDep
-from src.user import UserDep, TokenDep
+from src.user import User, UserDep, TokenDep, FormDep, fake_users_db, UserInDB, fake_hash_password, fake_decode_token
 from sqlmodel import func
 
 from fastapi.encoders import jsonable_encoder
@@ -43,14 +44,53 @@ async def lifespan(app: FastAPI):
 app = FastAPI(root_path="/api/v1", lifespan=lifespan)
 
 
-@app.get("/users/me")
-async def get_current_user(current_user: UserDep):
-    return current_user
+# @app.get("/users/me")
+# async def get_current_user(current_user: UserDep):
+#     return current_user
 
 
 @app.get("/items/")
 async def read_items(token: TokenDep):
     return {"token": token}
+
+
+async def get_current_user(token: TokenDep):
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Not authenticated",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@app.post("/token")
+async def login(form_data: FormDep):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(
+            status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(
+            status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user.username, "token_type": "bearer"}
+
+
+@app.get("/users/me")
+async def read_users_me(current_user: Annotated[User, Depends(get_current_active_user)]):
+    return current_user
 
 
 @app.get("/")
--- src/user.py
+++ src/user.py
@@ -4,6 +4,29 @@ from pydantic import BaseModel
 from fastapi import Depends, FastAPI
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
+
+
+def fake_hash_password(password: str):
+    return "fakehashed" + password
+
+
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
@@ -12,6 +35,26 @@ class User(BaseModel):
     email: str | None = None
     full_name: str | None = None
     disabled: bool | None = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def fake_decode_token(token):
+    user = get_user(fake_users_db, token)
+    return user
+
+
+def fake_decode_token(token):
+    user = get_user(fake_users_db, token)
+    return user
 
 
 def fake_decode_token(token):
@@ -27,3 +70,5 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
 UserDep = Annotated[User, Depends(get_current_user)]
 
 TokenDep = Annotated[str, Depends(oauth2_scheme)]
+
+FormDep = Annotated[OAuth2PasswordRequestForm, Depends()]
