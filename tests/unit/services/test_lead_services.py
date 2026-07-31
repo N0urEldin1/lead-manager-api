955b9cf54764441fe5a7b4c5fbf9ca8964176686
NourEldin Elkhatib <nimom6055@gmail.com>
2026-07-31 19:03:04 +0300

    Test create in lead_services

--- tests/unit/services/test_lead_services.py
+++ tests/unit/services/test_lead_services.py
@@ -4,8 +4,8 @@ from fastapi import HTTPException
 import pytest
 from sqlmodel import select
 
-from src.models.lead import Lead
-from src.services.lead_services import read_all, read
+from src.models.lead import Lead, LeadCreate
+from src.services.lead_services import read_all, read, create
 
 
 # read_all
@@ -364,3 +364,33 @@ def test_read_returns_none_when_not_found():
         )
 
         assert result.status_code == 404
+
+
+# create
+def test_create_creates_new_lead():
+
+    user = MagicMock()
+    user.user_id = 1
+
+    lead = MagicMock()
+    lead.name = "name"
+    lead.email = "email"
+    lead.company = "company"
+    lead.status = "status"
+
+    session = MagicMock()
+
+    result = create(user, lead, session)
+
+    session.add.assert_called_once()
+    session.commit.assert_called_once()
+    session.refresh.assert_called_once()
+
+    session.add.assert_called_once_with(result)
+    session.refresh.assert_called_once_with(result)
+
+    assert result.owner_id == user.user_id
+    assert result.name == lead.name
+    assert result.company == lead.company
+    assert result.status == lead.status
+    assert result.email == lead.email
