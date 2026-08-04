f7db75b7917165350531d9bf3d4ad2ba6d9f5b54
NourEldin Elkhatib <nimom6055@gmail.com>
2026-08-05 01:06:25 +0300

    Imporve conftest fixture setup and test getting leads

--- tests/integration/test_leads_int.py
+++ tests/integration/test_leads_int.py
@@ -5,35 +5,38 @@ from src.models.lead import Lead
 
 
 # Test database isolation using transactions and rollbacks
-def test_database_is_empty(test_session):
-    assert test_session.exec(select(Lead)).all() == []
+def test_database_is_empty(create_test_session):
+    assert create_test_session.exec(select(Lead)).all() == []
 
 
-def test_get_leads_unauthenticated_returns_401_int(unauthenticated_client):
+def test_get_leads_unauthenticated_returns_401_int(testing_session, fake_unauthenticated_user_headers):
 
-    response = unauthenticated_client.get("/leads")
+    response = testing_session.get(
+        "/leads", headers=fake_unauthenticated_user_headers)
 
     assert response.status_code == 401
 
 
-def test_get_leads_authenticated_returns_200_int(authenticated_client, active_auth_headers):
+def test_get_leads_authenticated_returns_200_int(testing_session, fake_authenticated_active_user_headers):
 
-    response = authenticated_client.get("/leads", headers=active_auth_headers)
+    response = testing_session.get(
+        "/leads", headers=fake_authenticated_active_user_headers)
 
     assert response.status_code == 200
 
 
-def test_get_leads_authenticated_inactive_returns_400_int(authenticated_client_inactive, inactive_auth_headers):
+def test_get_leads_authenticated_inactive_returns_400_int(testing_session, fake_unauthenticated_inactive_user_headers):
 
-    response = authenticated_client_inactive.get(
-        "/leads", headers=inactive_auth_headers)
+    response = testing_session.get(
+        "/leads", headers=fake_unauthenticated_inactive_user_headers)
 
     assert response.status_code == 400
 
 
-def test_get_leads_return_empty_list_int(authenticated_client, active_auth_headers):
+def test_get_leads_return_empty_list_int(testing_session, fake_authenticated_active_user_headers):
 
-    response = authenticated_client.get("/leads", headers=active_auth_headers)
+    response = testing_session.get(
+        "/leads", headers=fake_authenticated_active_user_headers)
 
     assert response.status_code == 200
     assert response.json() == {
@@ -48,29 +51,33 @@ def test_get_leads_return_empty_list_int(authenticated_client, active_auth_heade
     }
 
 
-def test_get_leads_return_one_lead(authenticated_client, active_auth_headers_with_data, fake_current_user_with_data):
+def test_get_leads_return_five_leads(testing_session, active_auth_headers_with_data, fake_current_user_with_data):
+
+    page_size = 5
 
     data = fake_current_user_with_data
 
-    response = authenticated_client.get(
-        "/leads", headers=active_auth_headers_with_data)
+    response = testing_session.get(
+        f"/leads?page_size={page_size}", headers=active_auth_headers_with_data)
 
     assert response.status_code == 200
-    assert response.json() == {
-        "data": [
-            {
-                "name": "Test Lead 1",
-                "company": "Test Company 1",
-                "email": "test1@example.com",
-                "status": "new",
-                "lead_id": data.lead1.lead_id
-            }
-        ],
-        "pagination": {
-            "page": 1,
-            "page_size": 10,
-            "item_count": 1,
-        },
-        "next_page": None,
-        "previous_page": None
-    }
+
+    data = response.json()
+    assert data["pagination"]["item_count"] == page_size
+    assert len(data["data"]) == page_size
+
+
+def test_get_leads_return_ten_leads(testing_session, active_auth_headers_with_data, fake_current_user_with_data):
+
+    page_size = 10
+
+    data = fake_current_user_with_data
+
+    response = testing_session.get(
+        f"/leads?page_size={page_size}", headers=active_auth_headers_with_data)
+
+    assert response.status_code == 200
+
+    data = response.json()
+    assert data["pagination"]["item_count"] == page_size
+    assert len(data["data"]) == page_size
