23bf8a82306355f81f92f1f6db198b5ac6d625f2
NourEldin Elkhatib <nimom6055@gmail.com>
2026-08-06 23:32:15 +0300

    Add second user fixture and test get leads and get single lead unauthorized

--- tests/integration/test_leads_int.py
+++ tests/integration/test_leads_int.py
@@ -88,3 +88,60 @@ def test_get_leads_return_five_leads(testing_session, fake_authenticated_active_
     assert data["data"][0]["name"] == "Test Lead 1"
     assert data["pagination"]["item_count"] == count
     assert len(data["data"]) == count
+
+
+def test_get_leads_return_only_current_user_leads(testing_session, fake_authenticated_active_user_headers, fake_current_user, second_fake_user, create_test_session):
+
+    # User 1 setup
+
+    count1 = 2
+
+    user1 = fake_current_user
+
+    create_leads(create_test_session, user1, count1)
+
+    # User 2 setup
+
+    count2 = 2
+
+    user2 = second_fake_user
+
+    create_leads(create_test_session, user2, count2)
+
+    response = testing_session.get(
+        "/leads", headers=fake_authenticated_active_user_headers)
+
+    assert response.status_code == 200
+
+    data = response.json()
+    assert data["data"][0]["name"] == "Test Lead 1"
+    assert data["data"][1]["name"] == "Test Lead 2"
+
+    assert data["data"][0]["name"] != "Test Lead 3"
+
+    assert data["pagination"]["item_count"] == count1
+    assert len(data["data"]) == count1
+
+
+def test_get_single_lead_unauthorized_returns_404_int(testing_session, fake_authenticated_active_user_headers, fake_current_user, second_fake_user, create_test_session):
+
+    # User 1 setup
+
+    count1 = 1
+
+    user1 = fake_current_user
+
+    leads_user1 = create_leads(create_test_session, user1, count1)
+
+    # User 2 setup
+
+    count2 = 1
+
+    user2 = second_fake_user
+
+    leads_user2 = create_leads(create_test_session, user2, count2)
+
+    response = testing_session.get(
+        f"/leads/{leads_user2[0].lead_id}", headers=fake_authenticated_active_user_headers)
+
+    assert response.status_code == 404
