7ef4d536147d37b79eb62f9561deebbb2bb6878a
NourEldin Elkhatib <nimom6055@gmail.com>
2026-08-09 01:29:00 +0300

    Change http401 util to raise instead of return error

--- src/utils/http401.py
+++ src/utils/http401.py
@@ -6,7 +6,7 @@ def error_401():
     # raise HTTPException(status_code=status_code, detail={
     #     "status_code": status_code, "error_details": "Unauthorized. Incorrect username or password"})
 
-    return HTTPException(
+    raise HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Unauthorized. Incorrect username or password",
         headers={"WWW-Authenticate": "Bearer"},
