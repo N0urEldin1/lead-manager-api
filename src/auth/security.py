c8807332e0e781bf8e7f6222cf5765aecc0408af
NourEldin Elkhatib <nimom6055@gmail.com>
2026-08-09 22:02:19 +0300

    Fix get_current_user is security allways raising error_401()

--- src/auth/security.py
+++ src/auth/security.py
@@ -82,22 +82,22 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
 async def get_current_user(token: Annotated[str | None, Depends(oauth2_scheme)], session: SessionDep):
     if token is None:
         error_401_unauthorized()
-    credentials_exception = error_401()
+    # credentials_exception = error_401()
     # try to create a payload object by decoding the token (created on logging in) using the secret decoding key and algorithm
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         # Get the user name from the key "sub" in the token dictionary
         username = payload.get("sub")
         if username is None:
-            raise credentials_exception
+            error_401()
         token_data = TokenData(username=username)
     except InvalidTokenError:
-        raise credentials_exception
+        error_401()
     username = token_data.username
     user = get_user_from_db(username, session)
     # user = get_user(fake_users_db, username=token_data.username)
     if user is None:
-        raise credentials_exception
+        error_401()
     return user
 
 
